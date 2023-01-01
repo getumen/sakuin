8ff0f6b64ee6f122aee180d9e15f98716af16225
getumen <n.yoshihiro.jp@gmail.com>
2023-01-01 16:55:18 +0900

    add analyzer constructor

--- analysis/analyzer.go
+++ analysis/analyzer.go
@@ -21,3 +21,26 @@ type Analyzer struct {
 	tokenizer    Tokenizer
 	tokenFilters []TokenFilter
 }
+
+func NewAnalyzer(
+	charFilters []CharFilter,
+	tokenizer Tokenizer,
+	tokenFilters []TokenFilter,
+) Analyzer {
+	return Analyzer{
+		charFilters:  charFilters,
+		tokenizer:    tokenizer,
+		tokenFilters: tokenFilters,
+	}
+}
+
+func (a Analyzer) Analyze(s string) token.TokenStream {
+	for _, c := range a.charFilters {
+		s = c.Filter(s)
+	}
+	tokenStream := a.tokenizer.Tokenize(s)
+	for _, f := range a.tokenFilters {
+		tokenStream = f.Filter(tokenStream)
+	}
+	return tokenStream
+}
