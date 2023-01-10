97418fd2b86de69ff784db1dab3231d23600e18d
getumen <n.yoshihiro.jp@gmail.com>
2023-01-10 19:49:48 +0900

    :bug: fix term cond simplfy bug (#21)

--- benchmark/search_wikipedia_test.go
+++ benchmark/search_wikipedia_test.go
@@ -7,7 +7,6 @@ import (
 
 	"github.com/getumen/sakuin/analysis"
 	"github.com/getumen/sakuin/analysis/charfilter"
-	"github.com/getumen/sakuin/analysis/tokenfilter"
 	"github.com/getumen/sakuin/analysis/tokenizer"
 	"github.com/getumen/sakuin/expression"
 	"github.com/getumen/sakuin/postinglist"
@@ -22,53 +21,71 @@ func BenchmarkSearchWikipedia(b *testing.B) {
 
 	indexPath := path.Join(tmpDir, "index")
 
-	japaneseTokenizer, err := tokenizer.NewJapaneseTokinizer()
-	require.NoError(b, err)
-
 	analyzer := analysis.NewAnalyzer(
 		[]analysis.CharFilter{
 			charfilter.NewUnicodeNFKCFilter(),
 		},
-		japaneseTokenizer,
-		[]analysis.TokenFilter{
-			tokenfilter.NewJapanaseStopwords(),
-		},
+		tokenizer.NewBigramTokenizer(),
+		[]analysis.TokenFilter{},
 	)
+
 	storage, err := lsmstorage.NewStorage(indexPath)
 	require.NoError(b, err)
 	defer storage.Close()
 
 	var hits, segmentNum int
+	docs := make(map[uint64]struct{})
 
-	for i := 0; i < b.N; i++ {
-		ctx := context.Background()
+	b.Run("search", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			ctx := context.Background()
 
-		tokenStream := analyzer.Analyze("奈良県")
-		terms := tokenStream.Terms()
-		phrase := make([]*expression.Expression, len(terms))
-		relativePosition := make([]uint32, len(terms))
-		for i := range terms {
-			phrase[i] = expression.NewFeature(expression.NewFeatureSpec(
-				"title", termcond.NewEqual(terms[i])))
-			relativePosition[i] = uint32(i)
+			tokenStream := analyzer.Analyze("日本")
+			terms := tokenStream.Terms()
+			titlePhrase := make([]*expression.Expression, len(terms))
+			bodyPhrase := make([]*expression.Expression, len(terms))
+			relativePosition := make([]uint32, len(terms))
+			for i := range terms {
+				titlePhrase[i] = expression.NewFeature(expression.NewFeatureSpec(
+					"title", termcond.NewEqual(terms[i])))
+				bodyPhrase[i] = expression.NewFeature(expression.NewFeatureSpec(
+					"body", termcond.NewEqual(terms[i])))
+				relativePosition[i] = uint32(i)
+			}
+
+			query := expression.NewOr(
+				[]*expression.Expression{
+					expression.NewPhrase(titlePhrase, relativePosition),
+					expression.NewPhrase(titlePhrase, relativePosition),
+				},
+			)
+			it, err := storage.GetIndexIterator(ctx, query.TermConditions())
+			require.NoError(b, err)
+
+			lists := make([]*postinglist.PostingList, 0)
+
+			var segNum int
+			for it.HasNext() {
+				value := it.Next()
+				lists = append(lists, value.Search(query))
+				segNum++
+			}
+
+			result := postinglist.Union(lists)
+			hits = result.Len()
+			segmentNum = segNum
+			cur := result.Cursor()
+			for {
+				docs[cur.Value().DocIDForTest()] = struct{}{}
+				if !cur.Next() {
+					break
+				}
+			}
 		}
+	})
 
-		query := expression.NewPhrase(phrase, relativePosition)
-		it, err := storage.GetIndexIterator(ctx, query.TermConditions())
-		require.NoError(b, err)
-
-		lists := make([]*postinglist.PostingList, 0)
-
-		var segNum int
-		for it.HasNext() {
-			value := it.Next()
-			lists = append(lists, value.Search(query))
-			segNum++
-		}
-
-		result := postinglist.Union(lists)
-		hits = result.Len()
-		segmentNum = segNum
-	}
 	b.Logf("%d hits in %d segment\n", hits, segmentNum)
+	for docID := range docs {
+		b.Logf("https://ja.wikipedia.org/w/index.php?curid=%d", docID)
+	}
 }
